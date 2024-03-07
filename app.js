c91d5c016d7aecdb1d80ab45cef9c526bb4589d7
Muhammad Rameez Khan <m.rameezkhan77@gmail.com>
2024-03-07 09:47:15 +0500

    added index.html and app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// Task 1
+// var var1, var2, var3;
+alert("hello");
+
+// Task 2
+//var 123, alert, var, hel lo , h-r
+//var hey , h123, dollar$ , newVar, h_r
+
+
+// Task 3
+// document.write("Rules for naming JS variables");
+// document.write(<p>"Variable names can only contain number,$,
+//  _and ,."</p>)
